cea033f565b4209ef5abb9b98289c44f4d049275
Jeff Rudolph <jeffrey.david.rudolph@gmail.com>
2023-09-20 18:59:59 -0400

    clean up function

--- src/components/Players/AbilityScore.jsx
+++ src/components/Players/AbilityScore.jsx
@@ -21,38 +21,6 @@ class AbilityScore extends Component {
   };
 
   calculateModifier = (abilityScore) => {
-    if (abilityScore === 1) {
-      return -5;
-    } else if (abilityScore >= 2 && abilityScore <= 3) {
-      return -4;
-    } else if (abilityScore >= 4 && abilityScore <= 5) {
-      return -3;
-    } else if (abilityScore >= 6 && abilityScore <= 7) {
-      return -2;
-    } else if (abilityScore >= 8 && abilityScore <= 9) {
-      return -1;
-    } else if (abilityScore >= 10 && abilityScore <= 11) {
-      return 0;
-    } else if (abilityScore >= 12 && abilityScore <= 13) {
-      return 1;
-    } else if (abilityScore >= 14 && abilityScore <= 15) {
-      return 2;
-    } else if (abilityScore >= 16 && abilityScore <= 17) {
-      return 3;
-    } else if (abilityScore >= 18 && abilityScore <= 19) {
-      return 4;
-    } else if (abilityScore >= 20 && abilityScore <= 21) {
-      return 5;
-    } else if (abilityScore >= 22 && abilityScore <= 23) {
-      return 6;
-    } else if (abilityScore >= 24 && abilityScore <= 25) {
-      return 7;
-    } else if (abilityScore >= 26 && abilityScore <= 27) {
-      return 8;
-    } else if (abilityScore >= 28 && abilityScore <= 29) {
-      return 9;
-    } else if (else {
-      return "Error";
-    }
+    return Math.floor((abilityScore - 10) / 2);
   };
 }
