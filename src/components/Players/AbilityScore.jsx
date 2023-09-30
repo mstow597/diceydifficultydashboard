0226a254623e90640e483a66a93dd9e557c0dd92
Jeff Rudolph <jeffrey.david.rudolph@gmail.com>
2023-09-30 12:51:30 -0400

    Changed class to func

--- src/components/Players/AbilityScore.jsx
+++ src/components/Players/AbilityScore.jsx
@@ -1,30 +1,31 @@
 import React, { Component } from "react";
-
+import "./AbilityScore.css";
 //Ability Score containers (Strength, Dex, etc), will calculate the modifier
 //needs a method on click to roll
 //needs a method for on hover to show a little pencil icon that you can then use to edit the number
 
-class AbilityScore extends Component {
-  constructor(props) {
-    super(props);
+function AbilityScore() {
+  const [abilityScore, setAbilityScore] = useState(0);
+  const [modifier, setModifier] = useState(0);
 
-    //instantiated state
-    this.state = {
-      abilityScore: 0,
-      modifier: 0,
-    };
-  }
-  //roll the dice
-  handleClick = () => {
+  const handleClick = () => {
     const randomNumber = Math.floor(Math.random() * 20) + 1;
-    const result = randomNumber + this.modifier;
+    const result = randomNumber + calculateModifier(abilityScore);
+    // You can do something with the 'result' here if needed.
   };
 
-  calculateModifier = (abilityScore) => {
+  const calculateModifier = (abilityScore) => {
     if (abilityScore < 1 || abilityScore > 30) {
       return "Error";
     }
-
     return Math.floor((abilityScore - 10) / 2);
   };
+
+  return (
+    <div className="box">
+      <div className="circle"></div>
+    </div>
+  );
 }
+
+export default AbilityScore;
